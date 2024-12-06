d2c18065b2a8ae54ba6645a382ed9717afa18ce3
diomedesm1 <diome.mo35ra.ult@gmail.com>
2024-12-06 08:16:50 -0300

    se modifico el cors

--- config/cors.php
+++ config/cors.php
@@ -17,18 +17,16 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
-
     'allowed_origins' => [
-    'http://localhost:5173', 
-    'https://apiprueba1-production.up.railway.app',
-    'https://timonm1.github.io/WebPrueba1/', 
-    'https://present-daryl-diomedesm1-5e0a99ab.koyeb.app', 
-    'https://timonm1.github.io.*',
-    'https://github.com/diomedesm1/apiPruebaWeb'
-],
+        'http://localhost:5173',
+        'https://apiprueba1-production.up.railway.app',
+        'https://timonm1.github.io/WebPrueba1',
+        'https://present-daryl-diomedesm1-5e0a99ab.koyeb.app',
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://timonm1.github.io.*'
+    ],
 
     'allowed_headers' => ['*'],
 
